34b5405e56e47790c0d5e7feaf4d629191f619c5
vibhansa-msft <vibhansa@microsoft.com>
2020-10-13 21:06:11 +0530

    Add comments to bazil fuse

--- blobfusego/fuseendpoint/bazilfuse/bazildir.go
+++ blobfusego/fuseendpoint/bazilfuse/bazildir.go
@@ -13,6 +13,7 @@ import (
 	"golang.org/x/net/context"
 )
 
+// Dir : Structure holding the Dir info for fuse
 type Dir struct {
 	path   string
 	dirlck sync.RWMutex
@@ -20,10 +21,12 @@ type Dir struct {
 	fs     *FS
 }
 
+// Attr : Get the attributes of the given directory
 func (d *Dir) Attr(ctx context.Context, o *fuse.Attr) error {
 	Logger.LogDebug("FD : Dir Attr called for %s", d.path)
 
 	if d.path == "" {
+		// Called for mount path
 		Logger.LogDebug("FD : Dir Attr called for mount point")
 		o.Inode = d.nodeid
 		o.Valid = time.Duration(*Config.BlobfuseConfig.AttrTimeOut)
@@ -42,9 +45,11 @@ func (d *Dir) Attr(ctx context.Context, o *fuse.Attr) error {
 	return nil
 }
 
+// Lookup : Check whether given object exists in the directory structure or not
 func (d *Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
 	Logger.LogDebug("FD : Dir Lookup called for %s : %s", d.path, name)
 
+	// Ignore certain linux standard things
 	if name == ".Trash" ||
 		name == ".Trash-1000" ||
 		name == ".xdg-volume-info" ||
@@ -55,26 +60,31 @@ func (d *Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
 	return &Dir{}, nil
 }
 
+// ReadDirAll : Get the list of objects from a directory
 func (d *Dir) ReadDirAll(ctx context.Context) (dirs []fuse.Dirent, err error) {
 	Logger.LogDebug("FD : Dir ReadDirAll called for %s", d.path)
 	return dirs, err
 }
 
+// Mkdir : Create a new directory
 func (d *Dir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (n fs.Node, err error) {
 	Logger.LogDebug("FD : Dir Mkdir called for %s", d.path)
 	return n, nil
 }
 
+// Create : Create a new node in directory
 func (d *Dir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (n fs.Node, h fs.Handle, err error) {
 	Logger.LogDebug("FD : Dir Create called for %s", d.path)
 	return n, h, nil
 }
 
+// Rename : Rename a directory
 func (d *Dir) Rename(ctx context.Context, req *fuse.RenameRequest, newDir fs.Node) error {
 	Logger.LogDebug("FD : Dir Rename called for %s", d.path)
 	return nil
 }
 
+// Remove : Delete a directory
 func (d *Dir) Remove(ctx context.Context, req *fuse.RemoveRequest) error {
 	Logger.LogDebug("FD : Dir Remove called for %s", d.path)
 	return nil
--- blobfusego/fuseendpoint/bazilfuse/bazilfile.go
+++ blobfusego/fuseendpoint/bazilfuse/bazilfile.go
@@ -15,31 +15,37 @@ type File struct {
 	fs     *FS
 }
 
+// Attr : Get the properties of a file
 func (f *File) Attr(ctx context.Context, o *fuse.Attr) error {
 	Logger.LogDebug("FD : File Attr called for %s", f.path)
 	return nil
 }
 
+// Setattr : Set the attributes of a given file
 func (f *File) Setattr(ctx context.Context, req *fuse.SetattrRequest, resp *fuse.SetattrResponse) error {
 	Logger.LogDebug("FD : File Setattr called for %s", f.path)
 	return nil
 }
 
+// Open : Open a file for read/write
 func (f *File) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenResponse) (h fs.Handle, err error) {
 	Logger.LogDebug("FD : File Open called for %s", f.path)
 	return h, err
 }
 
+// ReadAll : Read all the contenets of given file
 func (f *File) ReadAll(ctx context.Context) (data []byte, err error) {
 	Logger.LogDebug("FD : File ReadAll called for %s", f.path)
 	return data, err
 }
 
+// Write : Write data to a file
 func (f *File) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error {
 	Logger.LogDebug("FD : File Write called for %s", f.path)
 	return nil
 }
 
+// Flush : Flush data to file on close
 func (f *File) Flush(ctx context.Context, req *fuse.FlushRequest) error {
 	Logger.LogDebug("FD : File Flush called for %s", f.path)
 	return nil
